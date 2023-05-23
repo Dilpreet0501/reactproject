a19c541ed4b962a54312cb63d402d4be0fa85c73
Dilpreet0501 <100590445+Dilpreet0501@users.noreply.github.com>
2023-05-24 01:38:43 +0530

    second commit

--- src/Components/Basics/Navbar/Navbar.js
+++ src/Components/Basics/Navbar/Navbar.js
@@ -1,17 +1,38 @@
 import React from 'react'
+import { useState,useEffect,useRef } from 'react';
 import "./Navbar.scss"
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick,true);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick,true);
+    };
+  }, []);
   return (
     <>
-    <nav className="navbar">
+    <nav className={`navbar ${isDropdownOpen ? 'dropdown-open' : ''}`}>
     <div className='logo'>
-        <span className='name'> XivTech </span><br/>
+        <span className={`name ${isDropdownOpen ? 'open' : ''}`}> XivTech </span><br/>
         <span className='brand'>RPA Experts </span>
     </div>
     <ul className='nav-link'>
-    <div className='drop'>
-    <li className='link'>Services
-    <i class="fa fa-sharp fa-light fa-caret-down"></i></li>
+    <div className='drop'ref={dropdownRef}>
+    <li id='show' className={`link ${isDropdownOpen ? 'link-open' : ''}`}  onMouseEnter={handleDropdownToggle} >Services
+  <span className='dropdown'> <i class=" fa fa-thin fa-caret-down"></i></span> </li>
+  {isDropdownOpen && (
     <div className='content'>
      
        <div className='ai' >
@@ -26,9 +47,7 @@ const Navbar = () => {
        <img src='./images/a2.jpg' alt='build'></img>
        <caption >Build Apps - Deliver new apps users love <br/>Modernize Apps - Rearchitect valuable legacy apps</caption>
        </div></div>
-        
-      
-    
+
        <div className='cloud' >
         <span className='head'>Simplify Cloud Ops - Apply The New Rules Of Ops</span>  <div className='des'>
        <img src='./images/cl2.jpg' alt='cloud'></img>
@@ -41,15 +60,16 @@ const Navbar = () => {
        <caption >Azure <br/>
                  AWS</caption></div>
        </div>
-        </div> 
-    </div>
+        </div>)} 
+        </div>
  
-    <li className='link'>Career</li>
-    <li className='link'>About</li>
-    <li className='link'>Contact</li>
-    <i class="fas fa-solid fa-magnifying-glass" ></i>
-
-
+    <li className={`link ${isDropdownOpen ? 'link-open' : ''}`}  >Career</li>
+    <li className={`link ${isDropdownOpen ? 'link-open' : ''}`}  >About</li>
+    <li className={`link ${isDropdownOpen ? 'link-open' : ''}`}  >Contact</li>
+   <li className={`link ${isDropdownOpen ? 'link-open' : ''}`}><i class="fas fa-search"></i></li>
+   <div className='btn'>
+  <button className='talk'>Let's Talk</button>
+  </div>
 
     </ul>
   </nav>
